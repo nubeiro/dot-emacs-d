08e7fa1954f84f5b8c7e173411e0a8259ba5a3f3
Raul Araya <nubeiro@gmail.com>
2017-03-02 21:55:21 +0100

    continue refactor towards use-package, add another ref to README.

--- README.md
+++ README.md
@@ -2,23 +2,18 @@
 
 Inspired by [Chapter2 from Clojure for the Brave and True](http://www.braveclojure.com/basic-emacs/). 
 
-Evolved into my own thingy. 
+Evolved into my own thingy with use-package. 
 
 ## TODO ##
 * command-log?
-
 * customize for PHP editing. 
-    * pending packages: ac-php
-
 * customize for Python
-
 * org-mode setup
-
 * calendar setup
 
 ## References ##
 
 * https://gitlab.com/buildfunthings/emacs-config/blob/master/loader.org
 * http://pages.sachachua.com/.emacs.d/Sacha.html
-
-
+* https://github.com/waymondo/hemacs/blob/master/init.el
+* https://github.com/redguardtoo/mastering-emacs-in-one-year-guide
--- init.el
+++ init.el
@@ -118,10 +118,15 @@
 
 (use-package smex
   :ensure t
-  :defer t)
+  :bind ("M-x" . smex)
+  :init
+  (smex-initialize)
+  :config
+  (setq smex-save-file (concat user-emacs-directory ".smex-items")))
 
 (use-package projectile
-  :ensure t)
+  :ensure t
+  :init (projectile-global-mode))
 
 (use-package tagedit
   :ensure t)
@@ -170,6 +175,31 @@
   :ensure t
   :defer t)
 
+(use-package uniquify
+  :config (setq uniquify-buffer-name-style 'forward))
+
+(use-package recentf
+  :config
+  (recentf-mode 1)
+  (setq recentf-save-file (concat user-emacs-directory ".recentf")
+        recentf-max-menu-items 40))
+
+(use-package saveplace
+  :init (save-place-mode 1)
+  :config
+  (progn
+    (setq-default save-place t)
+    (setq save-place-file (concat user-emacs-directory "places")
+          save-place-limit nil)))
+
+(use-package js-mode
+  :mode 
+  (("\\.json$" . js-mode)
+   ("\\.js$" . js-mode))
+  :config
+  (setq js-indent-level 2)
+  (add-hook 'js-mode-hook 'subword-mode))
+
 (if (eq system-type 'darwin)
     (use-package exec-path-from-shell
       :ensure t
@@ -195,50 +225,48 @@
 ;; The forward naming method includes part of the file's directory
 ;; name at the beginning of the buffer name
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Uniquify.html
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
 
-;; Turn on recent file mode so that you can more easily switch to
-;; recently edited files when you first start emacs
-(setq recentf-save-file (concat user-emacs-directory ".recentf"))
-(require 'recentf)
-(recentf-mode 1)
-(setq recentf-max-menu-items 40)
 
 ;; Shows a list of buffers
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-;; Enhances M-x to allow easier execution of commands. Provides
-;; a filterable list of possible commands in the minibuffer
-;; http://www.emacswiki.org/emacs/Smex
-(setq smex-save-file (concat user-emacs-directory ".smex-items"))
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-;; projectile everywhere!
-(projectile-global-mode)
+(global-set-key (kbd "s-t") '(lambda () (interactive)))
+(global-set-key (kbd "M-/") 'hippie-expand)
+;; Interactive search key bindings. By default, C-s runs
+;; isearch-forward, so this swaps the bindings.
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(setq inhibit-startup-message t
-      inhibit-startup-echo-area-message t)  
+(show-paren-mode 1)
 (global-linum-mode)
+(global-hl-line-mode 1)
 
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 
 (set-face-attribute 'default nil :height 150)
-(setq initial-frame-alist '((top . 0) (left . 0) (width . 146) (height . 52)))
-(setq 
+(setq ring-bell-function 'ignore
       x-select-enable-clipboard t
       x-select-enable-primary t
       save-interprogram-paste-before-kill t
       apropos-do-all t
-      mouse-yank-at-point t)
+      mouse-yank-at-point t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t
+      initial-frame-alist '((top . 0) (left . 0) (width . 146) (height . 52))
+      ns-right-alternate-modifier nil
+      electric-indent-mode nil
+      auto-save-default nil
+      create-lockfiles nil)
+
+(setq-default indent-tabs-mode nil)
 (setq-default frame-title-format "%b (%f)")
-(global-set-key (kbd "s-t") '(lambda () (interactive)))
-(setq ring-bell-function 'ignore)
-;; Customizations relating to editing a buffer.
-;; Key binding to use "hippie expand" for text autocompletion
-;; http://www.emacswiki.org/emacs/HippieExpand
-(global-set-key (kbd "M-/") 'hippie-expand)
+;; shell scripts
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
 
 ;; Lisp-friendly hippie expand
 (setq hippie-expand-try-functions-list
@@ -247,39 +275,12 @@
         try-expand-dabbrev-from-kill
         try-complete-lisp-symbol-partially
         try-complete-lisp-symbol))
-;; enable right alt as alt key
-(setq ns-right-alternate-modifier nil)
-
-;; Highlights matching parenthesis
-(show-paren-mode 1)
-
-;; Highlight current line
-(global-hl-line-mode 1)
-
-;; Interactive search key bindings. By default, C-s runs
-;; isearch-forward, so this swaps the bindings.
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
-
-;; Don't use hard tabs
-(setq-default indent-tabs-mode nil)
-
-;; When you visit a file, point goes to the last place where it
-;; was when you previously visited the same file.
-;; http://www.emacswiki.org/emacs/SavePlace
-(require 'saveplace)
-(setq-default save-place t)
-;; keep track of saved places in ~/.emacs.d/places
-(setq save-place-file (concat user-emacs-directory "places"))
 
 ;; Emacs can automatically create backup files. This tells Emacs to
 ;; put all backups in ~/.emacs.d/backups. More info:
 ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Backup-Files.html
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
                                                "backups"))))
-(setq auto-save-default nil)
 
 ;; comments
 (defun toggle-comment-on-line ()
@@ -290,7 +291,6 @@
 
 ;; yay rainbows!
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-
 
 ;; use 2 spaces for tabs
 (defun die-tabs ()
@@ -307,45 +307,14 @@
       (ns-get-selection-internal 'CLIPBOARD)
     (quit nil)))
 
-(setq electric-indent-mode nil)
-
-;; (load "misc.el")
 ;; Changes all yes/no questions to y/n type
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; shell scripts
-(setq-default sh-basic-offset 2)
-(setq-default sh-indentation 2)
 
-;; No need for ~ files when editing
-(setq create-lockfiles nil)
-
-;; Go straight to scratch buffer on startup
-(setq inhibit-startup-message t)
-
-;; Languages 
-;; javascript / html
-(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
-(add-hook 'js-mode-hook 'subword-mode)
 (add-hook 'html-mode-hook 'subword-mode)
-(setq js-indent-level 2)
 (eval-after-load "sgml-mode"
   '(progn
      (require 'tagedit)
      (tagedit-add-paredit-like-keybindings)
      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(frame-background-mode (quote dark))
- '(package-selected-packages
-   (quote
-    (org org-ac use-package yaml-mode tagedit solarized-theme smex rainbow-delimiters projectile paredit markdown-mode magit ido-ubiquitous git-timemachine git-gutter geiser flycheck exec-path-from-shell company color-theme-sanityinc-tomorrow clojure-mode-extra-font-locking cider ac-php))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
