f4dfbdc34bc6ee06d0415ada8a13f80d772ade3f
Raul Araya <nubeiro@gmail.com>
2017-03-06 22:37:53 +0100

    Refine helm setup.

--- init.el
+++ init.el
@@ -270,24 +270,26 @@
   :init
   (progn
     (require 'helm-config)
-    (setq helm-candidate-number-limit 100)
-    ;; From https://gist.github.com/antifuchs/9238468
-    (setq helm-idle-delay 0.0 ; update fast sources immediately (doesn't).
-          helm-input-idle-delay 0.01  ; this actually updates things
-					; reeeelatively quickly.
-          helm-quick-update t
-          helm-M-x-requires-pattern nil
-          helm-ff-skip-boring-files t)
-    (helm-mode))
+    (helm-mode t))
   :config
   (ido-mode -1)
-  :bind (("C-c h" . helm-mini)
-		    ("C-h a" . helm-apropos)
-		    ("M-y" . helm-show-kill-ring)
-		    ("M-x" . helm-M-x)
-		    ("C-x c o" . helm-occur)
-		    ("C-x c s" . helm-swoop)
-		    ("C-x c SPC" . helm-all-mark-rings)))
+  (setq helm-candidate-number-limit 100
+        helm-idle-delay 0.1 
+        helm-input-idle-delay 0.1 
+        helm-quick-update t
+        helm-buffers-fuzzy-matching t
+        helm-recentf-fuzzy-match    t
+        helm-ff-auto-update-initial-value t
+        helm-M-x-requires-pattern nil
+        helm-ff-skip-boring-files t)
+  :bind (("C-x b" . helm-mini)
+         ("C-h a" . helm-apropos)
+         ("M-y" . helm-show-kill-ring)
+         ("M-x" . helm-M-x)
+         ("C-x c o" . helm-occur)
+         ("C-x c s" . helm-swoop)
+         ("C-x c SPC" . helm-all-mark-rings)
+         ("C-x C-f" . helm-find-files)))
 
 (use-package helm-projectile
   :ensure t
